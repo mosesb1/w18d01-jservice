30d49ce554477b38b933a9c8c69d9938053e99ae
Moses Burkhart <moses.burkhart1@gmail.com>
2022-04-08 17:49:17 -0400

    finished 10 random questions section

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,20 +1,28 @@
 import Button from "./Button";
 
 export default function Buttons({multipleData, showArray, setShowArray, classNames}){
-    const buttons = [];
-    multipleData.forEach((data, idx) => {
-        buttons.push(<Button classNames={classNames} text={!showArray[idx+1] ? data.answer : data.question} onClick={(evt) => {
-            setShowArray(showArray.map((value,i) => {
-                return(
-                    i === idx + 1 ? !value : value
-                )
-            }))
-        }}/>)
-    })
 
-    return (
-        <div className="multipleBtns">
-            {buttons}
-        </div>
-    )
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
+
+    const loaded = () => {
+        const buttons = [];
+        multipleData.forEach((data, idx) => {
+            buttons.push(<Button classNames={classNames} text={!showArray[idx+1] ? data.answer : data.question} key={idx} onClick={(evt) => {
+                setShowArray(showArray.map((value,i) => {
+                    return(
+                        i === idx + 1 ? !value : value
+                    )
+                }))
+            }}/>)
+        })
+    
+        return (
+            <div className="multipleBtns">
+                {buttons}
+            </div>
+        )
+    }
+    return multipleData ? loaded() : loading()
 }
