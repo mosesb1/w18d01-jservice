aa2673afcedfe46249116254c86e93e541861e6d
Moses Burkhart <moses.burkhart1@gmail.com>
2022-04-09 14:55:10 -0400

    minor tweak

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,15 +7,21 @@ export default function Buttons({multipleData, showArray, setShowArray, classNam
     }
 
     const loaded = () => {
-        const buttons = [];
-        multipleData.forEach((data, idx) => {
-            buttons.push(<Button classNames={classNames} text={!showArray[idx+1] ? data.answer : data.question} key={idx} onClick={(evt) => {
-                setShowArray(showArray.map((value,i) => {
-                    return(
-                        i === idx + 1 ? !value : value
-                    )
-                }))
-            }}/>)
+        const buttons = multipleData.map((data, idx) => {
+            return (
+                <Button 
+                    classNames={classNames} 
+                    text={!showArray[idx+1] ? data.answer : data.question} 
+                    key={idx} 
+                    onClick={(evt) => {
+                        setShowArray(showArray.map((value,i) => {
+                            return(
+                                i === idx + 1 ? !value : value
+                            )
+                        }))
+                    }}
+                />
+            )
         })
     
         return (
